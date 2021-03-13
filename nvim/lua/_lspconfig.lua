ca8fb0752c1b7188b92548be463627f0761a42d3
Alan Canniff <alan.canniff@pm.me>
2021-03-13 16:19:22 +1300

    add lua config for vhdl ls
    add a server implemenation and the key mappings
    delete the old plug in config. It's not used anymore

--- nvim/lua/_lspconfig.lua
+++ nvim/lua/_lspconfig.lua
@@ -1,6 +1,30 @@
 -- LSP settings
 local nvim_lsp = require('lspconfig')
 
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+local server_name = "vhdl_ls"
+
+configs[server_name] ={
+  default_config = {
+    cmd = {"vhdl_ls"};
+    filetypes = {"vhdl", "vhd"};
+    root_dir = util.root_pattern("vhdl_ls.toml");
+    docs = {
+      description = [[
+https://github.com/kraigher/rust_hdl
+
+language server for vhdl
+
+]];
+      default_config = {
+        root_dir = [[util.root_pattern("vhdl_ls.toml");]]
+      };
+    }
+  }
+}
+
 local on_attach = function(_client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
@@ -23,8 +47,11 @@ local on_attach = function(_client, bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 end
 
-local servers = { 'clangd' }
+local servers = { 'clangd', 'vhdl_ls' }
 
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup { on_attach = on_attach }
 end
+
+-- " command! Format lua vim.lsp.buf.formatting()<CR>
+-- " nnoremap <silent> \f    :Format<CR>
