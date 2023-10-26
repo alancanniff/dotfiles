528dbf1e67d16a3a52753d3198364a1e4c916ea7
Alan Canniff <alan.canniff@psonic.com.au>
2023-10-27 09:50:15 +1300

    vsg

--- vsg/rules/localized_019.py
+++ vsg/rules/localized_019.py
@@ -50,7 +50,7 @@ class rule_022(remove_spaces_before_token_rule):
         self.solution = "Ensure no space exists between case label and :"
 
 
-class rule_022(remove_spaces_before_token_rule):
+class rule_023(remove_spaces_before_token_rule):
     """
     Check there are no spaces betwen the block label and :
     """
@@ -60,3 +60,75 @@ class rule_022(remove_spaces_before_token_rule):
             self, "localized", "002", token.case_statement.label_colon
         )
         self.solution = "Ensure no space exists between case label and :"
+
+
+class rule_024(remove_spaces_before_token_rule):
+    """
+    Check there are no spaces betwen the constant and :
+    """
+
+    def __init__(self):
+        remove_spaces_before_token_rule.__init__(
+            self, "localized", "002", token.constant_declaration.colon
+        )
+        self.solution = "Ensure no space exists between case label and :"
+
+
+class rule_025(remove_spaces_before_token_rule):
+    """
+    Check there are no spaces betwen the constant and :
+    """
+
+    def __init__(self):
+        remove_spaces_before_token_rule.__init__(
+            self, "localized", "025", token.interface_constant_declaration.colon
+        )
+        self.solution = "Ensure no space exists between interface constant and :"
+
+
+class rule_026(remove_spaces_before_token_rule):
+    """
+    Check there are no spaces betwen the constant and :
+    """
+
+    def __init__(self):
+        remove_spaces_before_token_rule.__init__(
+            self, "localized", "026", token.interface_signal_declaration.colon
+        )
+        self.solution = "Ensure no space exists between interface signal and :"
+
+
+class rule_027(remove_spaces_before_token_rule):
+    """
+    Check there are no spaces betwen the constant and :
+    """
+
+    def __init__(self):
+        remove_spaces_before_token_rule.__init__(
+            self, "localized", "027", token.interface_variable_declaration.colon
+        )
+        self.solution = "Ensure no space exists between interface variable and :"
+
+
+class rule_028(remove_spaces_before_token_rule):
+    """
+    Check there are no spaces betwen the constant and :
+    """
+
+    def __init__(self):
+        remove_spaces_before_token_rule.__init__(
+            self, "localized", "028", token.interface_file_declaration.colon
+        )
+        self.solution = "Ensure no space exists between interface file and :"
+
+
+class rule_029(remove_spaces_before_token_rule):
+    """
+    Check there are no spaces betwen the constant and :
+    """
+
+    def __init__(self):
+        remove_spaces_before_token_rule.__init__(
+            self, "localized", "029", token.interface_unknown_declaration.colon
+        )
+        self.solution = "Ensure no space exists between interface unknown and :"
